a5dcdcddfb97925042da5999800507fbe26a1200
Shweta-A96 <reddyshweta96@gmail.com>
2024-12-28 20:35:02 +0530

    Added API001

---
+++ src/test/java/com/thetestingacademy/APITest001.java
@@ -0,0 +1,23 @@
+package com.thetestingacademy;
+
+import io.restassured.RestAssured;
+
+public class APITest001 {
+
+    public static void main(String[] args){
+
+        //Gherkins Syntax
+
+        //Full URL -> https://restful-booker.herokuapp.com/booking/1
+        //base URI -> https://restful-booker.herokuapp.com
+        //base path -> /booking/1
+        RestAssured
+                .given()
+                     .baseUri("https://restful-booker.herokuapp.com")
+                     .basePath("/booking/1")
+                .when()
+                     .get()
+                .then().log().all()
+                     .statusCode(200);
+    }
+}
